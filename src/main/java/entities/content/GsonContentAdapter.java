49ff3e48219d894f87d7a289ecee5ff2a1de8556
TomSuworof <rc1995king@gmail.com>
2020-08-13 15:33:33 +0300

    - fix(architecture): added interfaces - fix(contentAdapter): fixed path for non-IDEA environment

---
+++ src/main/java/entities/content/GsonContentAdapter.java
@@ -0,0 +1,127 @@
+package entities.content;
+
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/*
+ * this adapter allows us to use content, that will be automatically updated
+ */
+
+public class GsonContentAdapter implements ContentAdapter {
+    private static class Content {
+        private ArrayList<String> shouldDo;
+        private ArrayList<String> inProgress;
+        private ArrayList<String> done;
+    }
+
+    private static final String fileName = "contentMy.json";
+    private static String path = ClassLoader.getSystemResource(fileName).toString();
+    static {
+        if (path.startsWith("jar:")) {
+            path = fileName;
+        }
+        if (path.startsWith("file:")) {
+            path = path.substring(6);
+        }
+
+    }
+
+    private static Content content;
+
+    private Content getContent() {
+        System.out.println(path);
+
+        try (FileReader fileReader = new FileReader(path)) {
+            content = new Gson().fromJson(fileReader, Content.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return content;
+    }
+
+    public ArrayList<String> getShouldDo() {
+        return getContent().shouldDo;
+    }
+    public ArrayList<String> getDone() {
+        return getContent().done;
+    }
+    public ArrayList<String> getInProgress() {
+        return getContent().inProgress;
+    }
+
+    public void addShouldDo(String message) {
+        content = getContent();
+        content.shouldDo.add("- " + message);
+        update();
+    }
+    public void moveShouldDoToInProgress(int index) throws IndexOutOfBoundsException{
+        content = getContent();
+        content.inProgress.add(0, content.shouldDo.get(index));
+        content.shouldDo.remove(index);
+        update();
+    }
+    public void moveInProgressToDone(int index) throws IndexOutOfBoundsException {
+        content = getContent();
+        content.done.add(0, content.inProgress.get(index));
+        content.inProgress.remove(index);
+        update();
+    }
+    public void clearSomething(String message) {
+        content = getContent();
+        switch (message.toUpperCase()) {
+            case "SHOULD DO":
+                content.shouldDo.clear();
+                break;
+            case "IN PROGRESS":
+                content.inProgress.clear();
+                break;
+            case "DONE":
+                content.done.clear();
+                break;
+            case "all":
+                content.shouldDo.clear();
+                content.inProgress.clear();
+                content.done.clear();
+            default:
+                break;
+        }
+        update();
+    }
+    public void removeTask(String message) throws NumberFormatException {
+        content = getContent();
+        int index = Integer.parseInt(message.split(" ")[message.split(" ").length - 1]);
+        String nameOfColumn = message.substring(0, message.length() - String.valueOf(index).length() - 1);
+        try {
+            switch (nameOfColumn.toUpperCase()) {
+                case "SHOULD DO":
+                    content.shouldDo.remove(index);
+                    break;
+                case "IN PROGRESS":
+                    content.inProgress.remove(index);
+                    break;
+                case "DONE":
+                    content.done.remove(index);
+                    break;
+                default:
+                    break;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR. You do not have so mush tasks");
+        }
+        update();
+    }
+
+    private void update() {
+        String jsonString = new Gson().toJson(content);
+        try (FileWriter fileWriter = new FileWriter(path, false)) {
+            fileWriter.write(jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/entities/helpers/input/ConsoleWriter.java
@@ -0,0 +1,50 @@
+package entities.helpers.input;
+
+import entities.content.ContentAdapter;
+import entities.content.GsonContentAdapter;
+
+public class ConsoleWriter implements  Writer {
+
+    private static final ContentAdapter contentAdapter = new GsonContentAdapter();
+
+    public void newTask(String message) {
+        // adding new task in column 'do'
+        contentAdapter.addShouldDo(message);
+    }
+
+    public void pick(String index) {
+        // move i'th task to 'inProgress'
+        try {
+            contentAdapter.moveShouldDoToInProgress(Integer.parseInt(index));
+        } catch (IndexOutOfBoundsException indexEx) {
+            System.out.println("ERROR. You do not have so much tasks in SHOULD DO");
+        } catch (NumberFormatException numberEx) {
+            System.out.println("ERROR. It was not a number");
+        }
+    }
+
+    public void done(String index) {
+        // move i'th task tp 'done'
+        try {
+            contentAdapter.moveInProgressToDone(Integer.parseInt(index));
+        } catch (IndexOutOfBoundsException indexEx) {
+            System.out.println("ERROR. You do not have so much tasks in IN PROGRESS");
+        }catch (NumberFormatException numberEx) {
+            System.out.println("ERROR. It was not a number");
+        }
+    }
+
+    public void clear(String message) {
+        // clear certain column or all of them
+        contentAdapter.clearSomething(message);
+    }
+
+    public void remove(String message) {
+        // allow to remove certain element from columns
+        try {
+            contentAdapter.removeTask(message);
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR. It was not a number");
+        }
+    }
+}
